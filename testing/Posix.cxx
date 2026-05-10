61c24a473a394253e7119c77f2a20a5641fdef15
EJainDev <ekansh.jain2011@gmail.com>
2026-05-10 10:49:33 -0400

    fix(testing): make tests work
    Fix infinite recursion in fork and waitpid implementations as well as fixing call to test method.

--- testing/Posix.cxx
+++ testing/Posix.cxx
@@ -12,8 +12,8 @@ namespace cpputils::testing {
 #if defined(__unix__) || defined(__APPLE__)
 using pid_t = __pid_t;
 
-__pid_t fork() { return fork(); }
+__pid_t fork() { return ::fork(); }
 
-__pid_t waitpid(__pid_t pid, int* status, int options) { return waitpid(pid, status, options); }
+__pid_t waitpid(__pid_t pid, int* status, int options) { return ::waitpid(pid, status, options); }
 #endif
 }  // namespace cpputils::testing
--- testing/tests/Test.cpp
+++ testing/tests/Test.cpp
@@ -28,6 +28,10 @@ struct TestSuite {
 };
 
 int main(int argc, char** argv) {
+  if (argc > 1 && std::string(argv[1]) == "--list") {
+    return test<TestSuite>(argc, argv);
+  }
+
   std::vector elements{3, 5, 7};
 
   assertEqual(3, 3);
