20ae5a6c5614555f1e4fd4cb42174398382c64f6
EJainDev <ekansh.jain2011@gmail.com>
2026-06-21 16:52:59 -0400

    add better testing

--- tests/assertion_tests.cpp
+++ tests/assertion_tests.cpp
@@ -8,16 +8,17 @@ struct AssertionTestSuite {
   template <typename A = int, typename B = int>
   [[
     = Test{},
-    = Parameterize{tuple(3, 3), tuple(0, 0), tuple(-1, -1), tuple(1, 1),
-                   tuple(std::numeric_limits<int>::max(), std::numeric_limits<int>::max()),
-                   tuple(std::numeric_limits<int>::min(), std::numeric_limits<int>::min())}
-  ]] void AssertEqualInt(int a, int b) {
+    = makeTParamMatrix(
+        Pair<int, int>::InitM(
+            tuple(3, 3), tuple(0, 0), tuple(-1, -1), tuple(1, 1),
+            tuple(std::numeric_limits<int>::max(), std::numeric_limits<int>::max()),
+            tuple(std::numeric_limits<int>::min(), std::numeric_limits<int>::min())),
+        Pair<double, double>::InitM(
+            tuple(3.14, 3.14), tuple(0.0, 0.0), tuple(-1.0, -1.0), tuple(1.0, 1.0),
+            tuple(std::numeric_limits<double>::max(), std::numeric_limits<double>::max()),
+            tuple(std::numeric_limits<double>::min(), std::numeric_limits<double>::min())))
+  ]] void AssertEqual(A a, B b) {
     assertEqual(a, b);
-  }
-
-  template <typename T = float>
-  [[ = Test{}, = ParameterizeTemplate<int, double>{} ]] void AssertEqualTemplate() {
-    assertEqual(42, 42);
   }
 };
 
